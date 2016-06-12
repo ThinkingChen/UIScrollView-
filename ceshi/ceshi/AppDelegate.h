1c8936efac809603e8739527ab2d7f6c0391d22f
Rechard_chen <chensiwei.245@foxmail.com>
2016-06-13 01:14:51 +0800

    新建项目

---
+++ ceshi/ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by MAC on 16/6/13.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
